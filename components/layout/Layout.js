aff3463c16a0553171dec6519b48ca272e24bff7
Juan D. Jara <juanorigami@gmail.com>
2021-10-10 18:21:06 +0200

    some more tweaks

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,24 +1,21 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Image from 'next/image'
 import Nav from './Nav'
 import UserMenu from './UserMenu'
 import { CogIcon as SettingsIcon } from '@heroicons/react/solid'
 import NavLink from '@/components/common/NavLink'
+import config from '@/lib/config'
 
 export default function Layout({ children }) {
   return (
     <div className="relative flex flex-col text-white h-screen font-sans">
       <Head>
         <meta name="theme-color" content="#c0392b" />
-        <meta
-          name="description"
-          content="Portal para organizar las partidas de rol de la asociación Guardianes de Sevilla"
-        />
-        <title>Guardianes del Rol</title>
+        <meta name="description" content={config.appDescription} />
+        <title>{config.appTitle}</title>
       </Head>
       <div className="bgimage">
-        <Image src="/img/dice-bg.jpg" priority layout="fill" objectFit="cover" />
+        {/* <Image src="/img/dice-bg.jpg" priority layout="fill" objectFit="cover" /> */}
       </div>
       <div className="h-full md:flex align-stretch">
         <nav
@@ -26,7 +23,7 @@ export default function Layout({ children }) {
           className="md:pb-4 flex md:flex-col items-start justify-center md:bg-gray-700 md:bg-opacity-25">
           <Link href="/">
             <a className="h-20 hidden md:flex items-center px-4 hover:opacity-75">
-              <p className="text-3xl mr-4">Palomitas</p>
+              <p className="text-3xl mr-4 tracking-wider">InfiniTV</p>
               {/* <Image src="/img/index_bg.png" width={75} height={75} /> */}
             </a>
           </Link>
