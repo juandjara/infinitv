1df1e7d5953436fcfd3474492b849b355d5752b2
Juan D. Jara <juanorigami@gmail.com>
2021-10-11 02:07:58 +0200

    filters for tv series based on gdr system

--- pages/tv.js
+++ pages/tv.js
@@ -1,13 +1,16 @@
-import { SearchIcon } from '@heroicons/react/solid'
 import config from '@/lib/config'
 import useTVSeries from '@/lib/useTVSeries'
 import useTVGenres from '@/lib/useTVGenres'
 import { useEffect } from 'react'
+import { useQueryParams } from '@/lib/useQueryParams'
+import SearchBox from '@/components/common/SearchBox'
+import TVFiltersPanel from '@/components/tv/TVFiltersPanel'
 
 const tmdbImageURL = config.tmdbImageUrl
 
 export default function TV() {
-  const { data } = useTVSeries()
+  const { query } = useQueryParams()
+  const { data } = useTVSeries(query)
   const { genres } = useTVGenres()
 
   useEffect(() => {
@@ -16,20 +19,13 @@ export default function TV() {
 
   return (
     <main className="my-4 container mx-auto px-3">
-      <h1 className="mt-16 font-bold text-4xl leading-tight text-transparent bg-clip-text bg-gradient-to-br from-primary-700 to-primary-300">
-        Series
-      </h1>
-      <div className="relative mt-2 mb-6">
-        <SearchIcon
-          className="w-5 h-5 absolute top-1/2 left-4 text-gray-400"
-          style={{ transform: 'translateY(-50%)' }}
-        />
-        <input
-          type="search"
-          placeholder="Buscar en la lista"
-          className="w-full text-xl rounded-xl pl-12 pr-6 py-4 bg-white text-gray-700 placeholder-gray-500 placeholder-opacity-50"
-        />
-      </div>
+      <header className="mt-16 mb-2 flex">
+        <h1 className="flex-1 font-bold text-4xl leading-tight text-transparent bg-clip-text bg-gradient-to-br from-primary-700 to-primary-300">
+          Series
+        </h1>
+        <SearchBox route="/tv" />
+        <TVFiltersPanel />
+      </header>
       <ul className="grid grid-cols-cards gap-x-4 gap-y-4">
         {data.map(d => (
           <li key={d.id}>
@@ -43,7 +39,7 @@ export default function TV() {
 
 function VideoCard({ item, genres }) {
   function getGenreNames(ids) {
-    return genres.filter(g => ids.indexOf(g.id) !== -1).map(g => g.name);
+    return genres.filter(g => ids.indexOf(g.id) !== -1).map(g => g.name)
   }
 
   async function checkSonarr() {
@@ -69,7 +65,9 @@ function VideoCard({ item, genres }) {
         />
       </div>
       <div className="opacity-0 group-hover:opacity-100 transition-opacity flex flex-col justify-end absolute inset-0 w-full p-3 bg-gray-600 bg-opacity-60 rounded-xl">
-        <p className="text-lg font-medium">{new Date(item.first_air_date).getFullYear()}</p>
+        <p className="text-lg font-medium">
+          {item.first_air_date && new Date(item.first_air_date).getFullYear()}
+        </p>
         <p className="line-clamp-1">{getGenreNames(item.genre_ids).join(', ')}</p>
         <p className="text-2xl font-bold text-primary-100">{item.name}</p>
         <p className="mt-2 line-clamp-4">{item.overview}</p>
