fb88cdf4dc6bd38f469f7d2db2e6d06bfe5d4c26
Juan D. Jara <juanorigami@gmail.com>
2021-10-19 18:23:28 +0200

    video card to link in series

--- pages/tv.js
+++ pages/tv.js
@@ -4,8 +4,9 @@ import useTVGenres from '@/lib/tv/useTVGenres'
 import { useQueryParams } from '@/lib/useQueryParams'
 import SearchBox from '@/components/common/SearchBox'
 import TVFiltersPanel from '@/components/filters/TVFiltersPanel'
+import Button from '@/components/common/Button' 
 import { useRouter } from 'next/router'
-import Button from '@/components/common/Button'
+import Link from 'next/link'
 
 const tmdbImageURL = config.tmdbImageUrl
 
@@ -29,7 +30,7 @@ export default function TV() {
 
   return (
     <main className="my-4 container mx-auto px-3">
-      <header className="mt-16 mb-2 flex">
+      <header className="-mx-3 mt-16 mb-2 px-3 py-1 sticky top-0 z-10 flex">
         <h1 className="flex-1 font-bold text-4xl leading-tight text-transparent bg-clip-text bg-gradient-to-br from-primary-700 to-primary-300">
           Series
         </h1>
@@ -37,7 +38,9 @@ export default function TV() {
         <TVFiltersPanel />
       </header>
       <ul className="grid grid-cols-cards gap-x-4 gap-y-4">{pages}</ul>
-      <Button className="block mx-auto my-6" onClick={() => handlePageChange(page + 1)}>Cargar más</Button>
+      <Button className="block mx-auto my-6" onClick={() => handlePageChange(page + 1)}>
+        Cargar más
+      </Button>
     </main>
   )
 }
@@ -86,24 +89,26 @@ function VideoCard({ item, genres }) {
   // }
 
   return (
-    <div
-      className="h-full border border-gray-300 rounded-xl transition-transform transform-gpu scale-100 hover:scale-105 group relative"
-      style={{ minHeight: 240 }}>
-      <div className="h-full w-full">
-        <img
-          alt={item.name}
-          src={`${tmdbImageURL}/w300/${item.poster_path}`}
-          className="w-full h-full rounded-xl object-cover"
-        />
-      </div>
-      <div className="opacity-0 group-hover:opacity-100 transition-opacity flex flex-col justify-end absolute inset-0 w-full p-3 bg-gray-600 bg-opacity-60 rounded-xl">
-        <p className="text-lg font-medium">
-          {item.first_air_date && new Date(item.first_air_date).getFullYear()}
-        </p>
-        <p className="line-clamp-1">{getGenreNames(item.genre_ids).join(', ')}</p>
-        <p className="text-2xl font-bold text-primary-100">{item.name}</p>
-        <p className="mt-2 line-clamp-4">{item.overview}</p>
-      </div>
-    </div>
+    <Link href={`/tv/${item.id}`}>
+      <a
+        className="block relative h-full border border-gray-300 rounded-xl transition-transform transform-gpu scale-100 hover:scale-105 group"
+        style={{ minHeight: 240 }}>
+        <div className="h-full w-full">
+          <img
+            alt={item.name}
+            src={`${tmdbImageURL}/w300/${item.poster_path}`}
+            className="w-full h-full rounded-xl object-cover"
+          />
+        </div>
+        <div className="opacity-0 group-hover:opacity-100 transition-opacity flex flex-col justify-end absolute inset-0 w-full p-3 bg-gray-600 bg-opacity-60 rounded-xl">
+          <p className="text-lg font-medium">
+            {item.first_air_date && new Date(item.first_air_date).getFullYear()}
+          </p>
+          <p className="line-clamp-1">{getGenreNames(item.genre_ids).join(', ')}</p>
+          <p className="text-2xl font-bold text-primary-100">{item.name}</p>
+          <p className="mt-2 line-clamp-4">{item.overview}</p>
+        </div>
+      </a>
+    </Link>
   )
 }
